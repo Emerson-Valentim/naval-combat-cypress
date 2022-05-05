91d7a28351cbfc54b23c91aad1e0b19034792d5a
Emerson Valentim <emevalentim0@gmail.com>
2022-05-04 22:31:38 -0300

    feat: add profile commands

---
+++ cypress/fixtures/payloads/profile.ts
@@ -0,0 +1,42 @@
+import { image } from "../storage/data.json";
+
+const BASE_URL = Cypress.env("SERVER_BASE_URL");
+
+export const createProfilePicture = (data) => {
+
+  return {
+    url: `${BASE_URL}/FotoPerfil/CadastrarFoto`,
+    method: "POST",
+    failOnStatusCode: false,
+    body: {
+      CdIdUsuario: 0,
+      CdIdFotoPerfil: new Date().valueOf(),
+      DsFotoData: image,
+      ...data,
+    },
+  };
+};
+
+export const deleteProfilePicture = (data) => {
+  return {
+    url: `${BASE_URL}/FotoPerfil/ApagarFoto`,
+    method: "DELETE",
+    failOnStatusCode: false,
+    qs: {
+      CD_ID_USUARIO: 0,
+      ...data,
+    },
+  };
+};
+
+export const getProfilePicture = (data) => {
+  return {
+    url: `${BASE_URL}/FotoPerfil/ConsultarFoto`,
+    method: "GET",
+    failOnStatusCode: false,
+    qs: {
+      CD_ID_FOTO_PERFIL: 0,
+      ...data,
+    },
+  };
+};
