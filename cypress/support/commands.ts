c39adb487d775acb929eb0ca3674472cb6168736
Emerson Valentim <emevalentim0@gmail.com>
2022-05-07 15:50:20 -0300

    feat: define commands for cypress

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,6 +1,19 @@
 import Cache from "../fixtures/storage/cache";
 import { createUser, deleteUser, getUser } from "../fixtures/payloads/user";
-import { createProfilePicture, deleteProfilePicture, getProfilePicture } from "../fixtures/payloads/profile";
+import {
+  createProfilePicture,
+  deleteProfilePicture,
+  getProfilePicture,
+} from "../fixtures/payloads/profile";
+import {
+  createScenarioSkin,
+  createSkin,
+  deleteScenarioSkin,
+  deleteSkin,
+  getScenarioSkin,
+  getSkin,
+} from "../fixtures/payloads/skin";
+import { createMatch, deleteMatch, getMatch } from "../fixtures/payloads/match";
 
 const cache = Cache();
 
@@ -62,6 +75,87 @@ Cypress.Commands.add("deleteProfilePicture", (config) => {
 
 Cypress.Commands.add("getProfilePicture", (config) => {
   const payload = getProfilePicture(config?.data);
+
+  return cy.request({
+    ...payload,
+    ...config.options,
+  });
+});
+
+Cypress.Commands.add("createSkin", (config) => {
+  const payload = createSkin(config?.data);
+
+  return cy.request({
+    ...payload,
+    ...config?.options,
+  });
+});
+
+Cypress.Commands.add("deleteSkin", (config) => {
+  const payload = deleteSkin(config?.data);
+
+  return cy.request({
+    ...payload,
+    ...config?.options,
+  });
+});
+
+Cypress.Commands.add("getSkin", (config) => {
+  const payload = getSkin(config?.data);
+
+  return cy.request({
+    ...payload,
+    ...config.options,
+  });
+});
+
+Cypress.Commands.add("createScenarioSkin", (config) => {
+  const payload = createScenarioSkin(config?.data);
+
+  return cy.request({
+    ...payload,
+    ...config?.options,
+  });
+});
+
+Cypress.Commands.add("deleteScenarioSkin", (config) => {
+  const payload = deleteScenarioSkin(config?.data);
+
+  return cy.request({
+    ...payload,
+    ...config?.options,
+  });
+});
+
+Cypress.Commands.add("getScenarioSkin", (config) => {
+  const payload = getScenarioSkin(config?.data);
+
+  return cy.request({
+    ...payload,
+    ...config.options,
+  });
+});
+
+Cypress.Commands.add("createMatch", (config) => {
+  const payload = createMatch(config?.data);
+
+  return cy.request({
+    ...payload,
+    ...config?.options,
+  });
+});
+
+Cypress.Commands.add("deleteMatch", (config) => {
+  const payload = deleteMatch(config?.data);
+
+  return cy.request({
+    ...payload,
+    ...config?.options,
+  });
+});
+
+Cypress.Commands.add("getMatch", (config) => {
+  const payload = getMatch(config?.data);
 
   return cy.request({
     ...payload,
